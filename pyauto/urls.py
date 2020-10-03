baf8f8043f49d5a3eb369eb5ddf535f88a7415a1
souleymane <souleymanedialloo@yahoo.fr>
2020-10-03 18:37:40 +0200

    add app car

--- pyauto/urls.py
+++ pyauto/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("page.urls")),
+    path('cars', include("car.urls")),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
