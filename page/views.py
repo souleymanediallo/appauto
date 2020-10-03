7aa3c718f8574aab509a5e53d8a2c6badc5d8161
souleymane <souleymanedialloo@yahoo.fr>
2020-10-03 14:46:03 +0200

    add templates

--- page/views.py
+++ page/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "page/index.html")
+
+
+def about(request):
+    return render(request, "page/about.html")
+
+
+def service(request):
+    return render(request, "page/service.html")
+
+
+def contact(request):
+    return render(request, "page/contact.html")
